4eff6811690561a9cebf2ef8077b4080edeca542
Swapnil Marghade <sgmarghade@Swapnil-Marghade-MD4NN213Q3.local>
2024-05-03 13:21:36 +0530

    Length and find loop problem

---
+++ src/easy/length-of-last-word.ts
@@ -0,0 +1,50 @@
+/**
+ * Given a string s consisting of words and spaces, return the length of the last word in the string.
+ * A word is a maximal
+ * substring
+ *  consisting of non-space characters only.
+ * Example 1:
+ *
+ * Input: s = "Hello World"
+ * Output: 5
+ * Explanation: The last word is "World" with length 5.
+ * Example 2:
+ *
+ * Input: s = "   fly me   to   the moon  "
+ * Output: 4
+ * Explanation: The last word is "moon" with length 4.
+ * Example 3:
+ *
+ * Input: s = "luffy is still joyboy"
+ * Output: 6
+ * Explanation: The last word is "joyboy" with length 6.
+ */
+
+function lengthOfLastWord(s: string): number {
+    let i: number = s.length - 1;
+
+    //Start from end and reach to the end of word.
+    while (i >= 0) {
+        if (s[i] === ' ') {
+            i--;
+        } else {
+            break;
+        }
+    }
+
+    let endIndex = i;
+    while (i >= 0) {
+        if (s[i] !== ' ') {
+            i--;
+        } else {
+            i++;
+            break;
+        }
+    }
+
+    return endIndex - i + 1;
+}
+
+console.log(lengthOfLastWord('Hello World')); //5
+console.log(lengthOfLastWord('   fly me   to   the moon  ')); //4
+console.log(lengthOfLastWord('luffy is still joyboy')); //4
---
+++ src/medium/linked-list-cycle.ts
@@ -0,0 +1,33 @@
+/**
+ * Given head, the head of a linked list, determine if the linked list has a cycle in it.
+ * There is a cycle in a linked list if there is some node in the list that can be reached again by continuously following the next pointer. Internally, pos is used to denote the index of the node that tail's next pointer is connected to. Note that pos is not passed as a parameter.
+ * Return true if there is a cycle in the linked list. Otherwise, return false.
+ *
+ * Solution is to use 2 points. 1 fast and slow. Fast pointer move 2 steps and
+ * slow pointer move 1 step at a time. If there is any cycle both will
+ * meet somewhere.
+ * https://www.youtube.com/watch?v=RRSItF-Ts4Q
+ */
+
+// class ListNode {
+//     val: number
+//     next: ListNode | null
+//
+//     constructor(val?: number, next?: ListNode | null) {
+//         this.val = (val === undefined ? 0 : val)
+//         this.next = (next === undefined ? null : next)
+//     }
+// }
+
+function hasCycle(head: ListNode | null): boolean {
+    let fast: ListNode | null = head;
+    while (fast && fast.next) {
+        fast = fast.next.next;
+        head = head!.next;
+        if (fast === head) { //Both points to same ref.
+            return true;
+        }
+
+    }
+    return false;
+}
