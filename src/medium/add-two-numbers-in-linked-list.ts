b6db8f8128b0ad973c60637701f55d639c0d902f
Swapnil Marghade <sgmarghade@Swapnil-Marghade-MD4NN213Q3.local>
2024-05-03 13:59:06 +0530

    Add 2 sum in linkedList

---
+++ src/medium/add-two-numbers-in-linked-list.ts
@@ -0,0 +1,60 @@
+/**
+ * You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order, and each of their nodes contains a single digit. Add the two numbers and return the sum as a linked list.
+ * You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+ */
+
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+function addTwoNumbers(l1: ListNode | null, l2: ListNode | null): ListNode | null {
+    const firstArr: number[] = [];
+    const secondArr: number[] = [];
+    while (l1) {
+        firstArr.push(l1.val);
+        l1 = l1.next;
+    }
+    while (l2) {
+        secondArr.push(l2.val);
+        l2 = l2.next;
+    }
+    //console.log(firstArr, secondArr);
+    const finalArr: number[] = [];
+    let currentCarryOver = 0;
+    let firstArrIndex = 0;
+    let secondArrIndex = 0;
+    while (true) {
+        if (firstArrIndex >= firstArr.length && secondArrIndex >= secondArr.length) {
+            break;
+        }
+        const firstArrDigit: number = firstArrIndex < firstArr.length ? firstArr[firstArrIndex] : 0;
+        const secondArrDigit: number = secondArrIndex < secondArr.length ? secondArr[secondArrIndex] : 0;
+        const nextSum = firstArrDigit + secondArrDigit + currentCarryOver;
+        currentCarryOver = nextSum > 9 ? Math.floor(nextSum / 10) : 0;
+        finalArr.push(nextSum - 10 * currentCarryOver);
+        firstArrIndex++;
+        secondArrIndex++;
+    }
+    if (currentCarryOver > 0) {
+        finalArr.push(currentCarryOver);
+    }
+    if (!finalArr.length) {
+        return null;
+    }
+    let currentHead = new ListNode(finalArr[0]);
+    let returnHead = currentHead;
+    for (let returnIndex = 1; returnIndex < finalArr.length; returnIndex++) {
+        currentHead.next = new ListNode(finalArr[returnIndex]);
+        currentHead = currentHead.next;
+    }
+
+    return returnHead;
+}
