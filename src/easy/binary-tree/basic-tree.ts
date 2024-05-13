55f96fe616800ea3a4de9158ca6184a6bcf84530
Swapnil Marghade <s.g.marghade@gmail.com>
2024-05-13 12:55:02 +0530

    Implemented delete method

--- src/easy/binary-tree/basic-tree.ts
+++ src/easy/binary-tree/basic-tree.ts
@@ -2,6 +2,7 @@ export class Node {
     value: number;
     left: Node | null;
     right: Node | null;
+
     constructor(value: number) {
         this.value = value;
         this.left = null;
@@ -11,14 +12,17 @@ export class Node {
 
 class BinarySearchTree {
     root: Node | null;
+
     constructor() {
         this.root = null;
     }
 
+    //Check if tree is empty
     isEmpty(): boolean {
         return this.root === null;
     }
 
+    //Insert node in tree.
     insert(value: number) {
         const node = new Node(value);
         if (this.isEmpty()) {
@@ -28,6 +32,23 @@ class BinarySearchTree {
         }
     }
 
+    private insertNode(root: Node, newNode: Node) {
+        if (newNode.value <= root.value) {
+            if (root.left === null) {
+                root.left = newNode;
+            } else {
+                this.insertNode(root.left, newNode);
+            }
+        } else {
+            if (root.right === null) {
+                root.right = newNode;
+            } else {
+                this.insertNode(root.right, newNode);
+            }
+        }
+    }
+
+    //Search value in tree
     search(root: Node | null, valueToSearch: number): boolean {
         if (!root) {
             return false;
@@ -44,7 +65,7 @@ class BinarySearchTree {
         }
     }
 
-    //VLR
+    //DFS -> VLR
     preorderTraversal(root: Node | null) {
         if (root) {
             console.log(root.value);
@@ -52,7 +73,8 @@ class BinarySearchTree {
             this.preorderTraversal(root.right);
         }
     }
-    //LVR
+
+    //DFS -> LVR
     inorderTraversal(root: Node | null) {
         if (root) {
             this.inorderTraversal(root.left);
@@ -61,7 +83,7 @@ class BinarySearchTree {
         }
     }
 
-    //LRV
+    //DFS -> LRV
     postorderTraversal(root: Node | null) {
         if (root) {
             this.postorderTraversal(root.left);
@@ -69,21 +91,78 @@ class BinarySearchTree {
             console.log(root.value);
         }
     }
-    private insertNode(root: Node, newNode: Node) {
-        if (newNode.value <= root.value) {
-            if (root.left === null) {
-                root.left = newNode;
-            } else {
-                this.insertNode(root.left, newNode);
+
+    //BFS : Push root to queue.
+    //Read from queue print value and push it's left and right node to queue if exists.
+    levelOrder() {
+        const queue: (Node | null)[] = [];
+        queue.push(this.root);
+        while (queue.length) {
+            const current = queue.shift() as Node;
+            console.log(current.value);
+            if (current && current.left) {
+                queue.push(current.left);
             }
-        } else {
-            if (root.right === null) {
-                root.right = newNode;
-            } else {
-                this.insertNode(root.right, newNode);
+            if (current && current.right) {
+                queue.push(current.right)
             }
         }
     }
+
+    //Return min value of tree
+    min(root: Node): number {
+        if (!root?.left) {
+            return root.value;
+        }
+        return this.min(root.left);
+    }
+
+    //Return max value of tree
+    max(root: Node): number {
+        if (!root.right) {
+            return root.value;
+        }
+
+        return this.max(root.right);
+    }
+
+    /**
+     * Delete node
+     * 1. Node is leaf node -> No child. Just remove node
+     * 2. Node has 1 child node -> Remove node and replace it with child.
+     * 3. Node has 2 child nodes -> Replace value with least value from right
+     *    and delete least value node from right.
+     */
+    delete(value: number) {
+        this.root = this.deleteNode(this.root as Node, value);
+    }
+
+    private deleteNode(root: Node | null, value: number): Node | null {
+        if (!root) {
+            return root;
+        }
+
+        if (value < root.value) {
+            root.left = this.deleteNode(root.left, value);
+        } else if (value > root.value) {
+            root.right = this.deleteNode(root.right, value);
+        } else if (value === root.value) {
+            //Leaf node
+            if (root.left === null && root.right === null) {
+                return null;
+            } else if (root.left === null) {
+                return root.right;
+            } else if (root.right === null) {
+                return root.left;
+            }
+
+            root.value = this.min(root.right);
+            root.right = this.deleteNode(root.right, root.value);
+        }
+
+        return root;
+    }
+
 }
 
 const tree = new BinarySearchTree();
@@ -105,3 +184,16 @@ tree.inorderTraversal(tree.root);
 
 console.log('********** PostOrder *****************');
 tree.postorderTraversal(tree.root);
+
+console.log('********** LevelOrder / BFS *****************');
+tree.levelOrder();
+
+console.log('********** Min Value of tree *****************');
+console.log(tree.min(tree.root as Node));
+
+console.log('********** Max Value of tree *****************');
+console.log(tree.max(tree.root as Node));
+
+console.log('*********** Delete node 15 **********************');
+tree.delete(15);
+tree.inorderTraversal(tree.root);
