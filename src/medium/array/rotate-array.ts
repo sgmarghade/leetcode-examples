2589006c4d452a4bf83902b16871f4d3d6e27e47
Swapnil Marghade <s.g.marghade@gmail.com>
2024-05-15 16:19:35 +0530

    Rotate array using reversing array

--- src/medium/array/rotate-array.ts
+++ src/medium/array/rotate-array.ts
@@ -6,61 +6,40 @@
  * rotate 1 steps to the right: [7,1,2,3,4,5,6]
  * rotate 2 steps to the right: [6,7,1,2,3,4,5]
  * rotate 3 steps to the right: [5,6,7,1,2,3,4]
+ *
+ * Solution :
+ * 1. Reverse entire array : [7,6,5,4,3,2,1]
+ * 2. Reverse first k element again [5,6,7,1,2,3,4]
+ * 3. Reverse all other elements again.
+ *
  */
 
-function rotate(nums: number[], k: number): void {
-    let arrLength = nums.length;
-    if (arrLength <= 1) {
-        return;
+function rotate(nums: number[], inputK: number): void {
+    //First reverse entire arr
+    const k = inputK % nums.length;
+    let endIndex = nums.length - 1;
+    for(let i = 0; i < nums.length / 2; i++) {
+        const tmp = nums[endIndex - i];
+        nums[endIndex - i] = nums[i];
+        nums[i] = tmp;
     }
-    while(k > 0) {
-        //Rotate arr each time.
-        let replaceValue = nums[0];
-        for(let i = 1; i <= arrLength; i++) {
-            const tmpReplace = nums[i % arrLength];
-            nums[i % arrLength] = replaceValue;
-            replaceValue = tmpReplace;
-        }
-        k--;
+
+    //Rotate first part
+    endIndex = k - 1;
+    for(let i = 0; i < k / 2; i++) {
+        const tmp = nums[endIndex - i];
+        nums[endIndex - i] = nums[i];
+        nums[i] = tmp;
+    }
+
+    endIndex = nums.length - 1;
+    for (let i = 0; i < (nums.length - k) / 2; i++) {
+        const tmp = nums[endIndex - i];
+        nums[endIndex - i] = nums[i + k];
+        nums[i + k] = tmp;
     }
 }
 
 const arr = [1,2,3,4,5,6,7];
 rotate(arr, 3);
 console.log(arr); //    [5,6,7,1,2,3,4]
-
-// function inPlaceSolution(nums: number[], k: number): void {
-//     let arrLength = nums.length;
-//     if (arrLength <= 1) {
-//         return;
-//     }
-//     let finalDisplacement = k % arrLength;
-//     if (finalDisplacement === 0) {
-//         return;
-//     }
-//
-//     let outerLoopLimit = 1;
-//     let innerLoopHops = arrLength;
-//
-//     //If for length of 4 displacement is 2 then we have to hop each element once till it's back to 1st place
-//     // and do it for all elements in each hop
-//     if (arrLength % finalDisplacement === 0) {
-//         outerLoopLimit = finalDisplacement;
-//         innerLoopHops = arrLength / finalDisplacement;
-//     }
-//     for (let outerLoopStartIndex = 0; outerLoopStartIndex < outerLoopLimit; outerLoopStartIndex++) {
-//         let replaceValue = nums[outerLoopStartIndex];
-//         let replaceIndex = outerLoopStartIndex;
-//         for (let i = 0; i < innerLoopHops; i++) {
-//             const tmpReplaceIndex = (replaceIndex + finalDisplacement) % arrLength;
-//             const tmpReplace = nums[tmpReplaceIndex];
-//             nums[tmpReplaceIndex] = replaceValue;
-//             replaceValue = tmpReplace;
-//             replaceIndex = tmpReplaceIndex;
-//             if (replaceIndex === 0 && i !== 0 && outerLoopLimit === 1) {
-//                 replaceIndex = 1;
-//                 replaceValue = nums[replaceIndex];
-//             }
-//         }
-//     }
-// }
